64602bf868fc0729daad1ba28db05503f89a07a3
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-11-09 16:39:40 +0530

    Updated GUI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 
-class Application(tk.Frame):
+class Application(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
@@ -9,12 +9,19 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
+        style = ttk.Style()
+        style.configure("TButton",
+                        foreground="midnight blue",
+                        background="white",
+                        font=("Helvetica", 16, "bold"),
+                        padding=10)
+
+        self.hi_there = ttk.Button(self, style="TButton")
         self.hi_there["text"] = "Start Face Recognition"
         self.hi_there["command"] = self.start_face_recognition
         self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        self.quit = ttk.Button(self, text="QUIT", style="TButton",
                               command=self.master.destroy)
         self.quit.pack(side="bottom")
 

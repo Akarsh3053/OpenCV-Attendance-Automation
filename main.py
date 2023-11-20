343c73a84b6ad9263f4c6f69c6e1202f6b0dc58b
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-11-20 21:17:01 +0530

    created new main application body

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+# Loading mode graphics for attendance system
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+
+
+
+while True:
+    success, img = cap.read()
+
+    imgBackground[162:162+480, 55:55+640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
+
+    cv2.imshow("IRIS: Attendance Module", imgBackground)
+    cv2.waitKey(1)

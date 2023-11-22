3f4d424cbdfbbaf49a4e8a15a0ecd753c092ab53
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-11-22 19:58:52 +0530

    added Student image storage

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,14 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials, storage, db
+
+cred = credentials.Certificate("admin-service-key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://iris-attendance-module-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    'storageBucket': 'iris-attendance-module.appspot.com'
+})
 
 # Importing Student Images
 
@@ -12,6 +20,11 @@ studentIds = []
 for path in faceImages:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 
 # Encoding Generation
 
@@ -32,5 +45,3 @@ faceEncodings_withId = [encodeListKnown, studentIds]
 file = open("FaceEncodings.p", 'wb')
 pickle.dump(faceEncodings_withId, file)
 file.close()
-
-
